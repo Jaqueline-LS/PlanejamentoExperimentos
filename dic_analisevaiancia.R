2dbefafabfdaefce5f12f5e81970f84236a13f76
Jaqueline-LS <jaquelinelamas@ice.ufjf.br>
2024-11-25 20:37:18 -0300

    Programação DIC obtenção e análise de resíduos

--- dic_analisevaiancia.R
+++ dic_analisevaiancia.R
@@ -1,3 +1,4 @@
+rm(list = ls(all = TRUE))
 library("ExpDes") #utiliza a restrição sum(ti)=0
 library(readr)
 estacas <- read_table("estacas.txt")
@@ -18,9 +19,78 @@ boxcox((Estacas+0.05)~Cultivar) # eliminar o zero
 
 
 y.ij<-matrix(Estacas, byrow=T,ncol=5, nrow=4)
+I<-dim(y.ij)[1]
+J<-dim(y.ij)[2]
 
-m.hat<- sum(y_ij)/prod(dim(y.ij))
+m.hat<- sum(y.ij)/(I*J)
 
-ti.hat<-apply(y.ij, 1,sum)
+ti.hat<-apply(y.ij, 1,FUN=function(x)mean(x)-m.hat)
+# Restrição soma de ti.hat é igual zero
 
+residuo<-y.ij-m.hat-matrix(rep(ti.hat,J),byrow = F, ncol=J, nrow = I )
+
+residuo.squared<-residuo^2
+
+si<-apply(residuo.squared,1,FUN=function(x)sum(x)/I) #variância no trat i
+
+sum(si)/I
+
+s.2<-sum(residuo^2)/(I*(J-1)) # variância média
+
+s<-sqrt(s.2)
+
+residuo.padronizado<-(residuo-0)/s
+
+qqnorm(residuo.padronizado)
+qqline(residuo.padronizado)
+
+
+# Transformação
+
+ajustados<-m.hat+matrix(rep(ti.hat,J),byrow = F, ncol=J, nrow = I)
+plot(ajustados,residuo, pch=19)
+
+# Estabilizar a variância
+# Para transformações pode usar as funcoes/pacote
+boxcox((Estacas+0.05)~Cultivar) # eliminar o zero
+# Dados de contagem geralmente é raiz quadrada    
+# Lição de casa fazer a transformação e anilisar os resíduos
+
+Estacas.sqrt<-Estacas^(1/2)
+
+
+
+y.ij<-matrix(Estacas.sqrt, byrow=T,ncol=5, nrow=4)
+I<-dim(y.ij)[1]
+J<-dim(y.ij)[2]
+
+m.hat<- sum(y.ij)/(I*J)
+
+ti.hat<-apply(y.ij, 1,FUN=function(x)mean(x)-m.hat)
+# Restrição soma de ti.hat é igual zero
+
+residuo<-y.ij-m.hat-matrix(rep(ti.hat,J),byrow = F, ncol=J, nrow = I )
+
+residuo.squared<-residuo^2
+
+si<-apply(residuo.squared,1,FUN=function(x)sum(x)/I) #variância no trat i
+
+sum(si)/I
+
+s.2<-sum(residuo^2)/(I*(J-1)) # variância média
+
+s<-sqrt(s.2)
+
+residuo.padronizado<-(residuo-0)/s
+
+qqnorm(residuo.padronizado)
+qqline(residuo.padronizado)
+
+ajustados<-m.hat+matrix(rep(ti.hat,J),byrow = F, ncol=J, nrow = I )
+plot(ajustados, residuo, pch=19)
+
+hist(residuo, freq=F, col="#dcedc1", xlim=c(-3,3))
+curve(dnorm,-3,3, add=T)
+
+shapiro.test(residuo)
 
