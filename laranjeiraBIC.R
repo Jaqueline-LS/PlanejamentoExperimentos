9c84a6d6db18e48352ff42e1cc871ec6b92dfc06
Jaqueline Lamas da Silva <107161463+Jaqueline-LS@users.noreply.github.com>
2025-01-22 12:00:22 -0300

    Add files via upload

--- laranjeiraBIC.R
+++ laranjeiraBIC.R
@@ -1,6 +1,9 @@
 library("ExpDes")
 data(ex2)
 attach(ex2)
+library("dplyr")
+
+
 laranjeira<-matrix(c(145,155,166,
                      200,190,190,
                      183,186,208,
@@ -21,8 +24,21 @@ p<-c(145,155,166,
         206,165,170,
         250,271,230,
         164,190,193)
-b<-c(rep(1,9), rep(2,9), rep(3,9))
+b<-rep(1:3,9)
+
 t<-rep(1:9,rep(3,9))
 
 dados<-data.frame(prod=p, bloco=b, trat=t)
 sem.parcela.perdida<-rbd(treat=dados$prod, block=dados$bloco, resp=dados$prod)
+
+
+# parcela perdida = 192.5625 calculo nas anotações
+
+
+library(readr)
+bloco <- read_table("bloco.txt") 
+rbd(bloco$trat, bloco$bloco, bloco$resp)
+
+rbd(bloco$trat, bloco$bloco, bloco$resp_1)# uma parcela perdida
+
+rbd(bloco$trat, bloco$bloco, bloco$resp_2)# duas parcelas perdida
